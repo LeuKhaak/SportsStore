e2b05f1efab070a3566ab571ed1f71ea3ce937cc
Lev <lyoha.bel2020@gmail.com>
2022-04-28 16:41:10 +0300

    rest.datasource.ts error

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,7 +2,28 @@ import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
 import { Cart } from './cart.model';
+import { Order } from './order.model';
+import { OrderRepository } from './order.repository';
+import { RestDataSource } from './rest.datasource';
+import { HttpModule } from '@angular/http';
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart],
+  imports: [HttpModule],
+  providers: [
+    ProductRepository,
+    Cart,
+    Order,
+    OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource },
+  ],
 })
 export class ModelModule {}
+//@NgModule({
+//  providers: [
+//    ProductRepository,
+//    StaticDataSource,
+//    Cart,
+//    Order,
+//    OrderRepository,
+//  ],
+//})
+//export class ModelModule {}
